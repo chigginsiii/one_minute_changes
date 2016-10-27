8fd661baf4d78d2b9452ab31dcaf76fe61828f0d
Creighton Higgins <creighton.higgins@gmail.com>
2016-10-26 23:52:29 -0700

    random pair button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,87 @@
+// true if there's less than 2 selected
+var can_select_chord = function() {
+  if ($('#chord-selector a.selected').length < 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// display this chord
+var add_to_selected_chords = function(chord) {
+  var content = '<h2>' + chord.text() + '</h2>';
+  $('#selected-chords div:empty').first().append(content);
+}
+
+// undisplay this chord
+var remove_from_selected_chords = function(chord) {
+  $('#selected-chords div h2').filter(function(){
+    return $(this).text() === chord.text()
+  }).remove();
+};
+
+// feedback: can't select this button
+var flash_no_select = function($chord) {
+  var oldBG = $chord.css('background-color');
+  $chord.animate({
+    backgroundColor: "#fcc"
+  }, 300).animate({
+    backgroundColor: oldBG
+  }, 300, function() {
+    $(this).removeAttr('style');          
+  });
+}
+
+// click handler for chord buttons
+var toggleChord = function(evt) {
+  var $chord = $(this);
+  if ($chord.hasClass('selected')) {
+    $chord.removeClass('selected');
+    remove_from_selected_chords($chord);
+  } else {
+    if ( can_select_chord() ) {
+      $chord.addClass('selected');
+      // put this in #selected-chords
+      add_to_selected_chords($chord);
+    } else {
+      flash_no_select($chord);
+    }
+  }
+}
+
+var clearSelections = function() {
+  $('#chord-selector .selected').each(function(){
+    $(this).click();
+  });
+};
+
+var shuffle = function(chords) {
+  var curIndex = chords.length, swapIndex, tmp;
+
+  while (curIndex > 1) {
+    curIndex -= 1;
+    swapIndex = Math.floor(Math.random() * curIndex);
+
+    console.log('swapping ' + curIndex + ' with ' + swapIndex);
+
+    tmp = chords[swapIndex];
+    chords[swapIndex] = chords[curIndex];
+    chords[curIndex] = tmp;
+  }
+  return chords;
+}
+
+var randomPair = function() {
+  clearSelections();
+  chords = $('#chord-selector a');
+  shuffle(chords);
+
+  console.log(chords);
+
+  chords[0].click();
+  chords[1].click();
+}
+
 $(function(){
   // A Am A7 B B7 C C7 D Dm D7 E Em E7 E7highD F Fmaj7 G G7
   var chords = [
@@ -10,67 +94,25 @@ $(function(){
     'G', 'G7'
   ];
 
-  // true if there's less than 2 selected
-  var can_select_chord = function() {
-    if ($('#chord-selector a.selected').length < 2) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  // display this chord
-  var add_to_selected_chords = function(chord) {
-    var content = '<h2>' + chord.text() + '</h2>';
-    $('#selected-chords div:empty').first().append(content);
-  }
-
-  // undisplay this chord
-  var remove_from_selected_chords = function(chord) {
-    $('#selected-chords div h2').filter(function(){
-      return $(this).text() === chord.text()
-    }).remove();
-  };
-
-  // feedback: can't select this button
-  var flash_no_select = function($chord) {
-    var oldBG = $chord.css('background-color');
-    $chord.animate({
-      backgroundColor: "#fcc"
-    }, 300).animate({
-      backgroundColor: oldBG
-    }, 300, function() {
-      $(this).removeAttr('style');          
-    });
-  }
-
-  // click handler for chord buttons
-  var toggle_chord = function(evt) {
-    var $chord = $(this);
-    if ($chord.hasClass('selected')) {
-      $chord.removeClass('selected');
-      remove_from_selected_chords($chord);
-    } else {
-      if ( can_select_chord() ) {
-        $chord.addClass('selected');
-        // put this in #selected-chords
-        add_to_selected_chords($chord);
-      } else {
-        flash_no_select($chord);
-      }
-    }
-  }
-
   // setup chord buttons
   $chordSelector = $('#chord-selector');
+
   $.each(chords, function(i, chord_name){
     // make the link
     var $chord = $('<a href="#">' + chord_name + '</a>');
-    $chord.click(toggle_chord);
+    $chord.click(toggleChord);
     // add it to a list item
     var $item = $('<li></li>');
     $item.append($chord);
     // append to #chord-selector
     $chordSelector.append($item);
   });
+
+  // setup clear buttons
+  $clearButton = $('#clear-selections');
+  $clearButton.click(clearSelections);
+
+  // setup random button
+  $randomButton = $('#random-pair');
+  $randomButton.click(randomPair);
 });
